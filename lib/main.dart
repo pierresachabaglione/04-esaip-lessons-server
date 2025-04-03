b2e9b72c624fb2ef8295644ea452b61384f79366
pierresachabaglione <psbpro@hotmail.fr>
2025-04-03 17:14:09 +0200

    Added the websocket server (websocket_server.dart) Added the device registration process in the database handler (database_functions.dart)
    Added the database & websocket test in database_functions_test.dart and websocket_test_things_registration.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-// SPDX-FileCopyrightText: 2025 Benoit Rolandeau <benoit.rolandeau@allcircuits.com>
-//
-// SPDX-License-Identifier: MIT
-
 import 'package:esaip_lessons_server/managers/global_manager.dart';
 import 'package:esaip_lessons_server/ui/main_app/main_app_ui.dart';
+import 'package:esaip_lessons_server/websocket/websocket_server.dart';
 import 'package:flutter/material.dart';
 
 /// Main function to run the app
@@ -11,9 +8,14 @@ Future<void> main() async {
   // This method forces all initialization async functions to be finished before running the app.
   // This way, we can launch functions at init before the UI is started.
   // Ths UI starts after these functions are finished.
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await GlobalManager.instance.initialize();
+
+  // Initialize and start the WebSocket server
+  final webSocketServer = WebSocketServer();
+  await webSocketServer.start();
 
   runApp(const MainAppUi());
 }
